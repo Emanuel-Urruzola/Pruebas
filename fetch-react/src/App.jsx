7fd50f4a48413726a833e7aa6684567715eafd55
Emanuel Urruzola Alegre <emaurruzola@gmail.com>
2023-11-09 03:19:23 -0300

    Mostrar todas los datos de la api con un llamada con el objeto fetch de window

---
+++ fetch-react/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [post, setPost] = useState()
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => setPost(data))
+  },[])
+
+  return (
+    <>
+      <ul>
+        {
+          post &&
+          post.map(({title, body, id})=>{
+            return(
+              <li id={id} key={id}>
+                <h2>{title}</h2>
+                <p>{body}</p>
+              </li>
+          )})
+        }
+      </ul>
+    </>
+  )
+}
+
+export default App
